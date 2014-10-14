46eab92a7b8065c751fc23b8ab73dcc5bd8631b9
jiapeixin <458105816@qq.com>
2014-10-14 10:49:02 +0800

    Initial Commit

---
+++ TestProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestProject
+//
+//  Created by Jpxin on 14-10-14.
+//  Copyright (c) 2014年 Jpxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestProject/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TestProject
+//
+//  Created by Jpxin on 14-10-14.
+//  Copyright (c) 2014年 Jpxin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
